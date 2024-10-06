8af52d1c96a7816af11075fe2f5ed7dd5b329932
PrestoGuys <prestoguys@PrestoDebian.PrestoDebian>
2024-10-06 15:59:28 -0500

    made it so search link came from json, added SearchURLS

--- js/search.js
+++ js/search.js
@@ -1,11 +1,5 @@
 const thmasInputText = document.querySelector('#thmas-input-text');
-data = `
-{
-    "DuckDuckGo": "https://duckduckgo.com/?q=",
-    "Debian": "https://search.debian.org/?q=",
-    "Stack Overflow": "https://stackoverflow.com/search?q="
-}
-`
+data = '{"Google":"https://www.google.com/search?q=","Yahoo":"https://search.yahoo.com/search?p=","DuckDuckGo":"https://duckduckgo.com/?q=","Bing":"https://www.bing.com/search?q=","America Online":"https://search.aol.com/aol/search?q=","Swisscows":"https://swisscows.com/en/web?query=","Yandex":"https://yandex.com/search/?text=","Stack Overflow":"https://stackoverflow.com/search?q=","Github":"https://github.com/search?q=","Lifewire":"https://www.lifewire.com/search?q=","Debian":"https://search.debian.org/?q=","Redhat":"https://www.redhat.com/en/search?search=","Puppy Linux Forums":"https://forum.puppylinux.com/search.php?keywords=","Distrowatch":"https://distrowatch.com/table.php?distribution=","Youtube":"https://www.youtube.com/results?search_query=","Twitter":"https://twitter.com/search?q=","Reddit":"https://www.reddit.com/search/?q=","TikTok":"https://www.tiktok.com/search?q=","New York Times":"https://www.nytimes.com/search?query=","Google News":"https://news.google.com/search?q=","Link":"https://","Wikipedia":"https://en.wikipedia.org/w/index.php?search=","Scratch":"https://scratch.mit.edu/search/projects?q=","Internet Archive":"https://archive.org/search?query="}'
 
 
 
@@ -13,8 +7,7 @@ data = `
 
     var result = JSON.parse(data);
 
-function searchit(e) {
-
+function search(e) {
     if((e && e.keyCode == 13) || e == 0) {
     
         var e = document.getElementById("choo");
@@ -27,114 +20,5 @@ function searchit(e) {
         var URL = urlg + thmasInputText.value;
         console.log(URL);
         window.open(URL, "_blank");
-
-        /*
-        if (value === "google") {
-            var URL = "https://www.google.com/search?q=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "yahoo") {
-            var URL = "https://search.yahoo.com/search?p=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-    
-        
-        if (value === "stacover") {
-            var URL = "https://stackoverflow.com/search?q=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "bing") {
-            var URL = "https://www.bing.com/search?q=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "link") {
-            var URL = "https://" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "aol") {
-            var URL = "https://search.aol.com/aol/search?q=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "swissc") {
-            var URL = "https://swisscows.com/en/web?query=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "yout") {
-            var URL = "https://www.youtube.com/results?search_query=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "twitter") {
-            var URL = "https://twitter.com/search?q=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "nyt") {
-            var URL = "https://www.nytimes.com/search?query=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-
-        if (value === "goon") {
-            var URL = "https://news.google.com/search?q=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-
-        if (value === "redhat") {
-            var URL = "https://www.redhat.com/en/search?search=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "plf") {
-            var URL = "https://forum.puppylinux.com/search.php?keywords=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-
-        if (value === "diswat") {
-            var URL = "https://distrowatch.com/table.php?distribution=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-
-        if (value === "reddit") {
-            var URL = "https://www.reddit.com/search/?q=" + thmasInputText.value;
-            var win = window.open(URL, "_blank");
-        }
-        
-        if (value === "tik") {
-            var URL = "https://www.tiktok.com/search?q=" + thmasInputText.value;
-            window.open(URL, "_blank");
-        }
-
-        if (value === "lifewire") {
-            var URL = "https://www.lifewire.com/search?q=" + thmasInputText.value;
-            window.open(URL, "_blank");
-        }
-
-        if (value === "github") {
-            var URL = "https://github.com/search?q=" + thmasInputText.value;
-            window.open(URL, "_blank");
-        }
-
-        if (value === "wikiped") {
-            var URL = "https://en.wikipedia.org/w/index.php?search=" + thmasInputText.value;
-            window.open(URL, "_blank");
-        }
-        
-        if (value === "yandex") {
-            var URL = "https://yandex.com/search/?text=" + thmasInputText.value;
-            window.open(URL, "_blank");
-        }
-
-        if (value === "scratch") {
-            var URL = "https://scratch.mit.edu/search/projects?q=" + thmasInputText.value;
-            window.open(URL, "_blank");
-        }
-        */
     }
- 
 }
